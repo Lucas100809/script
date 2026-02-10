802cc330db9669bedb1c86f5ad6e54b5a26e835d
Lucas <aodrnilucas74@gmail.com>
2026-02-10 13:25:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+let nome = "Lucas";
+console.log(nome);
+
+
+let sobrenome = "Adorni";
+console.log(nome + " " + sobrenome);
+
+
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+
+let escola = "Minha Escola";
+console.log(escola);
+
+
+let a = "JavaScript é";
+let b = " legal";
+console.log(a + b);
+
+
+console.log("Bem-vindo " + nome);
+
+
+let texto = "Programar";
+console.log(texto.length);
+
+
+console.log("oi".toUpperCase());
+
+
+console.log("OI".toLowerCase());
+
+
+let cidade = "São Paulo";
+console.log(nome + " mora em " + cidade);
+
+
+
+
+let n1 = 10;
+let n2 = 2;
+console.log(n1 + n2);
+
+
+console.log(n1 - n2);
+
+
+console.log(n1 * n2);
+
+
+console.log(n1 / n2);
+
+
+
+
+let ativo = true;
+console.log(ativo);
+
+let numero = 15;
+console.log(numero > 10);
+
+
+let idade = 18;
+console.log(idade >= 18);
